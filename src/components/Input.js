4c3fb9c4548365c5652a9d8ebbf26eeba9411428
julia <julichis@mail.ru>
2023-04-15 22:53:40 +0400

    fixed input onchange props

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
-const Input = ({title, placeholder}) => {
+const Input = ({title, placeholder, onChange}) => {
   return (
     <label className={styles.input}>
       <div className={styles.input__label}>{title}</div>
-      <input placeholder={placeholder} className={styles.input__body} />
+      <input placeholder={placeholder} className={styles.input__body} onChange={onChange}/>
     </label>
   )
 }
